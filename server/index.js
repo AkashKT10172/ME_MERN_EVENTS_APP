604c5deb077d7ca3505e6d517068767b57b1fddf
Akash Kumar Tiwary <128840791+AkashKT10172@users.noreply.github.com>
2025-07-08 11:29:14 +0530

    cors issue fix.

--- server/index.js
+++ server/index.js
@@ -22,7 +22,12 @@ connectDB();
 startStatusUpdateCron();
 emailReminderCronJob();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://the-social-hub.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true,
+}));
+app.options('*', cors());
 app.use(express.json());
 
 app.use('/api/auth', ipBasedRateLimiter, authRoutes);
